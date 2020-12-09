bd7ae8db74d879bc634ff87d13689b05c85e2db4
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2020-12-09 18:01:48 +0300

    Добавлена простая валидация

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,38 @@
-﻿(function () {
-    'use strict'
+﻿function validate() {
 
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation')
+    event.preventDefault();
 
-        // Loop over them and prevent submission
-        Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                    alert("Неверный ввод данных")
-                }
-                form.classList.add('was-validated')
-            }, false)
-        })
-    }, false)
-}())
+    var name = document.form.name;
+    var lastname = document.getElementById("last");
+
+    var e = document.getElementById("day");
+    var day = document.getElementById("day").options[e.selectedIndex].value;
+
+    var indexMonth = document.getElementById("month").selectedIndex;
+    var month = document.getElementById("month").options[indexMonth].text;
+
+    var indexYear = document.getElementById("month").selectedIndex;
+    var year = document.getElementById("year").options[indexYear].text;
+    
+    if (name.value.length > 10 || name.value.length < 1) {
+        name.style.borderColor = "red";
+    }
+
+    if (lastname.value.length > 10 || lastname.value.length < 1) {
+        lastname.style.borderColor = "red";
+    }
+
+    if (!parseInt(day) && !parseInt(year) && month === "Месяц") {
+       
+    }
+    else {
+        if (!parseInt(day))
+            document.getElementById("day").style.borderColor = "red";
+        if (!parseInt(year))
+            document.getElementById("year").style.borderColor = "red";
+        if (month === "Месяц")
+            document.getElementById("month").style.borderColor = "red";
+    }
+
+    return true;
+}
